8929d0b189d2b731947c8eed5e42238eb98bd608
Matthew <mbrimmer1@gmail.com>
2016-05-14 14:40:46 -0400

    Updated the AI, but it still has some bugs in it.

--- tictactoe.js
+++ tictactoe.js
@@ -21,24 +21,96 @@ function firstMove () {
   turn = turn + 1;
 }
 
-// function playerOccupiesSquares (player, squares, board) {
-//   for (var i = 0; i < squares.length; i++) {
-//     var idx = squares[i];
-//     var value = board[idx];
-//     if (value !== player) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function computerCheckMove (player, squares, board) { /* this function checks each winning combo and places a N into array if it is
+ occupied by the player and returns each array to the calling function. Modeled after playerOccupiesSquares */
+  var bestMoveId = [];
+  var numOccupiedSquares = 0;
+  for (var i = 0; i < squares.length; i++) {
+    var idx = squares[i];
+    var value = board[idx];
+    if (value === player) {
+      bestMoveId.push("N");
+    } else {
+        bestMoveId.push(idx);
+      }
+    }
+    return bestMoveId;
+}
+
+function computerMakeMove (board) {
+  var moveCount = 0;
+  var rightMove;
+  for (var i = 0; i < winningCombos.length; i++) {
+    var combo = winningCombos[i];
+    var moves = computerCheckMove("X", combo, board) // passes "O" to player
+      for (var i = 0; i < moves.length; i++) {
+        if ( moves[i] === "N") {
+          moveCount = moveCount + 1;
+        } else if (moves[i] !== "N") {
+          rightMove = moves[i];
+        }
+      }
+      if (moveCount === 2) {
+        if (rightMove === 0) {
+          $('#A1').text("X");
+        } else if (rightMove === 1) {
+          $('#A2').text("X");
+        } else if (rightMove === 2) {
+          $('#A3').text("X");
+        } else if (rightMove === 3) {
+          $('#B1').text("X");
+        } else if (rightMove === 4) {
+          $('#B2').text("X");
+        } else if (rightMove === 5) {
+          $('#B3').text("X");
+        } else if (rightMove === 6) {
+          $('#C1').text("X");
+        } else if (rightMove === 7) {
+          $('#C2').text("X");
+        } else if (rightMove === 8) {
+          $('#C3').text("X");
+        }
+      }
+    var moveCount = 0
+    var moves = computerCheckMove("O", combo, board) // passes "X" to player
+    for (var i = 0; i < moves.length; i++) {
+      if ( moves[i] === "N") {
+        moveCount = moveCount + 1;
+      } else if (moves[i] !== "N") {
+        rightMove = moves[i];
+      }
+    }
+    if (moveCount === 2) {
+      if (rightMove === 0) {
+        $('#A1').text("O");
+      } else if (rightMove === 1) {
+        $('#A2').text("O");
+      } else if (rightMove === 2) {
+        $('#A3').text("O");
+      } else if (rightMove === 3) {
+        $('#B1').text("O");
+      } else if (rightMove === 4) {
+        $('#B2').text("O");
+      } else if (rightMove === 5) {
+        $('#B3').text("O");
+      } else if (rightMove === 6) {
+        $('#C1').text("O");
+      } else if (rightMove === 7) {
+        $('#C2').text("O");
+      } else if (rightMove === 8) {
+        $('#C3').text("O");
+      }
+    }
+    }
+  }
 
 function checkWinner (board) {
   for (var i = 0; i < winningCombos.length; i++) {
     var combo = winningCombos[i];
-    if (playerOccupiesSquares("O", combo, board)) {
+    if (playerOccupiesSquares("O", combo, board)) { // passes "O" to player
       return "O";
     }
-    if (playerOccupiesSquares("X", combo, board)) {
+    if (playerOccupiesSquares("X", combo, board)) { // passes "X" to player
       return "X"
     }
   }
@@ -47,8 +119,8 @@ function checkWinner (board) {
 
 function playerOccupiesSquares (player, squares, board) {
   for (var i = 0; i < squares.length; i++) {
-    var idx = squares[i];
-    var value = board[idx];
+    var idx = squares[i]; //squares are winning combos [i]
+    var value = board[idx]; //
     if (value !== player) {
       return false;
     }
@@ -83,8 +155,10 @@ $(function () {
       if (turn === 1) {
         firstMove ();
       }
-      var board = getCurrentMoves();
-      var winner = checkWinner (board);
+      if (turn !== 1 && turn % 2 === 1) {
+        var board = getCurrentMoves();
+        var winner = checkWinner (board);
+    }
       if (winner) {
         $('#display-box')
         .text('The winner is ' + winner)
