4cd4e5f8629bcd0d5458de317f48f4b55499f455
Matthew <mbrimmer1@gmail.com>
2016-05-16 21:26:11 -0400

    Made another update to the AI! Almost complete!

--- tictactoe.js
+++ tictactoe.js
@@ -12,69 +12,92 @@ var winningCombos = [
 ];
 function firstMove () {
   var moves =  getCurrentMoves ();
-  var move = moves[4];
-  if (move === ""){
+  if (moves[4] === "O"){
     $('#B2').text("X")
-  } else {
+  } else if ()
     $('#A1').text("X")
   }
 }
 
-function computerCheckMove (player, squares, board) { /* this function checks each winning combo and places a N into array if it is
+function nextMove () {
+  var moves = getCurrentMoves ();
+  var move = moves[8];
+  if (move === "O") {
+    $('#A3').text("X")
+  } else {
+    $('#C3').text("X")
+  }
+}
+
+function computerCheckMove (squares, board) { /* this function checks each winning combo and places a N into array if it is
  occupied by the player and returns each array to the calling function. Modeled after playerOccupiesSquares */
   var bestMoveId = [];
   var numOccupiedSquares = 0;
   for (var i = 0; i < squares.length; i++) {
     var idx = squares[i];
     var value = board[idx];
-    if (value === player) {
-      bestMoveId.push("O");
-    } else {
-        bestMoveId.push(idx);
-      }
+    if (value === "X") {
+      bestMoveId.push(value);
+    } else if ( value === "O") {
+      bestMoveId.push(value);
+
+      } else {
+          bestMoveId.push(idx);
     }
+  }
     return bestMoveId;
 }
-
 function computerMakeMoveX (board) {
   for (var i = 0; i < winningCombos.length; i++) {
     var moveCount = 0;
     var rightMove;
+    var totalMove = 0;
     var combo = winningCombos[i];
-    var moves = computerCheckMove("X", combo, board) // passes "O" to player
+    var moves = computerCheckMove(combo, board) // passes "O" to player
       for (var j = 0; j < moves.length; j++) {
         if ( moves[j] === "X") {
           moveCount = moveCount + 1;
-        } else if (moves[j] !== "X") {
+          totalMove = totalMove + 1;
+        } else if (moves[j] !== "X" && moves[j] !== "O") {
           rightMove = moves[j];
+          totalMove = totalMove + 1;
         }
-      if (moveCount === 2) {
+      if (moveCount === 2 && totalMove === 3) {
         if (rightMove === 0) {
           $('#A1').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 1) {
           $('#A2').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 2) {
           $('#A3').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 3) {
           $('#B1').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 4) {
           $('#B2').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 5) {
           $('#B3').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 6) {
           $('#C1').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 7) {
           $('#C2').text("X");
+          turn = turn + 1;
           return;
         } else if (rightMove === 8) {
           $('#C3').text("X");
+          turn = turn + 1;
           return;
         }
         return;
@@ -87,42 +110,54 @@ function computerMakeMoveO (board) {
 for (var i = 0; i < winningCombos.length; i++) {
   var rightMove = 0;
   var moveCount = 0;
+  var totalMove = 0;
   var combo = winningCombos[i];
-  var moves = computerCheckMove("O", combo, board) // passes "X" to player
+  var moves = computerCheckMove(combo, board) // passes "X" to player
   for (var j = 0; j < moves.length; j++) {
     if ( moves[j] === "O") {
       moveCount = moveCount + 1;
-    } else if (moves[j] !== "O") {
+      totalMove = totalMove + 1;
+    } else if (moves[j] !== "O" && moves[j] !== "X") {
       rightMove = moves[j];
+      totalMove = totalMove + 1;
     }
   }
-  if (moveCount === 2) {
+  if (moveCount === 2 && totalMove === 3) {
     if (rightMove === 0) {
       $('#A1').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 1) {
       $('#A2').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 2) {
       $('#A3').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 3) {
       $('#B1').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 4) {
       $('#B2').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 5) {
       $('#B3').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 6) {
       $('#C1').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 7) {
       $('#C2').text("X");
+      turn = turn + 1;
       return;
     } else if (rightMove === 8) {
       $('#C3').text("X");
+      turn = turn + 1;
       return;
     }
     return;
@@ -183,20 +218,21 @@ $(function () {
         firstMove ();
         turn = turn + 1;
       }
-      // if (turn % 2 === 1) {
-      //   var board = getCurrentMoves();
-      //   var bestMove = computerMakeMoveX (board);
-      //   turn = turn + 1;
-      // }
+      if (turn % 2 === 1) {
+        var board = getCurrentMoves();
+        var bestMove = computerMakeMoveX (board);
+      }
       if (turn % 2 === 1) {
         var board =getCurrentMoves();
         var bestMove = computerMakeMoveO (board);
-        turn = turn + 1;
       }
-      if (turn !== 1 && turn % 2 === 1) {
+      if (turn % 2 === 1) {
+        nextMove ();
+        turn = turn +1;
+      }
+      if (turn !== 1) {
         var board = getCurrentMoves();
         var winner = checkWinner (board);
-        turn = turn + 1;
     }
       if (winner) {
         $('#display-box')
